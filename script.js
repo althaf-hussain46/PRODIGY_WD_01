dff5a2b188d977b513f6fa8e3719612243c2c81d
althaf-hussain46 <althafhussain2k3@gmail.com>
2024-05-22 12:20:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-      navbar.style.backgroundColor = '#333';
-    } else {
-      navbar.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-    }
-  });
-  
